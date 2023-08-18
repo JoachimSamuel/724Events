b21823a7625eed6004a524d6787ef8ba0f5990e5
Joachim Samuel <joachimsamuel@gmail.com>
2023-08-18 14:30:24 +0200

    Correction de la fonction getMonth() et la callback onSuccss

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,51 +7,65 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+  
+  // Vérification si data et data.focus sont définis
+  const byDateDesc = data?.focus?.slice().sort((evtA, evtB) =>
+    new Date(evtA.date) < new Date(evtB.date) ? 1 : -1
   );
-  const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
-  };
+
   useEffect(() => {
-    nextCard();
-  });
+    const interval = setInterval(() => {
+      setIndex((prevIndex) =>
+        prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0
+      );
+    }, 5000);
+
+    return () => {
+      clearInterval(interval); // Nettoyer l'intervalle lors du démontage du composant
+    };
+  }, [byDateDesc]);
+
+  const handleRadioChange = (radioIdx) => {
+    setIndex(radioIdx);
+  
+  };
+
+  if (!byDateDesc || byDateDesc.length === 0) {
+    return null; // Si byDateDesc est vide, ne rien afficher
+  }
+
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+      {byDateDesc.map((event, idx) => (
+        <div
+          key={event.title} // Utilisation d'une propriété unique comme clé
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
       ))}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc.map((radioEvent, radioIdx) => (
+            <input
+              key={`${radioEvent.title}`}
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+              onChange={() => handleRadioChange(radioIdx)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
